828754a9963ea8a6a223efcaed032214cbdf4ad0
LoV432 <monib619@gmail.com>
2024-04-22 20:09:34 +0500

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,13 +12,27 @@ import Link from 'next/link';
 
 export default function Header() {
 	return (
-		<header className="sticky mb-2 flex w-full border-b border-muted bg-background px-5 pt-2">
-			<NavigationMenu>
-				<NavigationMenuList className="gap-2">
+		<header className="sticky top-0 z-10 mb-2 w-full border-b border-muted bg-background px-5 py-2">
+			<NavigationMenu className="gap-2">
+				<Link href="/" legacyBehavior passHref>
+					<NavigationMenuLink>
+						<Image
+							priority
+							className="h-6 w-6"
+							src="/logo.png"
+							alt="logo"
+							width={24}
+							height={24}
+						/>
+					</NavigationMenuLink>
+				</Link>
+				<NavigationMenuList>
 					<NavigationMenuItem>
 						<Link href="/" legacyBehavior passHref>
-							<NavigationMenuLink className={navigationMenuTriggerStyle()}>
-								<Image src="/logo.png" alt="logo" width={40} height={40} />
+							<NavigationMenuLink
+								className={`${navigationMenuTriggerStyle()} text-center text-xl font-bold`}
+							>
+								Ventscape Archive
 							</NavigationMenuLink>
 						</Link>
 					</NavigationMenuItem>
