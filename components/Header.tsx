dd0f33d60824b402b6369f59684f7095dd23db47
LoV432 <monib619@gmail.com>
2024-04-23 20:29:11 +0500

    add search button in navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ import Link from 'next/link';
 export default function Header() {
 	return (
 		<header className="sticky top-0 z-10 mb-2 w-full border-b border-muted bg-background px-5 py-2">
-			<NavigationMenu className="gap-2">
+			<NavigationMenu className="w-full max-w-[initial] gap-2">
 				<Link href="/" legacyBehavior passHref>
 					<NavigationMenuLink>
 						<Image
@@ -37,6 +37,19 @@ export default function Header() {
 						</Link>
 					</NavigationMenuItem>
 				</NavigationMenuList>
+				<Link href="/search" legacyBehavior passHref>
+					<NavigationMenuLink
+						className={`${navigationMenuTriggerStyle()} ml-auto text-center text-xl font-bold`}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="ionicon h-6 w-6 fill-white"
+							viewBox="0 0 512 512"
+						>
+							<path d="M464 428L339.92 303.9a160.48 160.48 0 0030.72-94.58C370.64 120.37 298.27 48 209.32 48S48 120.37 48 209.32s72.37 161.32 161.32 161.32a160.48 160.48 0 0094.58-30.72L428 464zM209.32 319.69a110.38 110.38 0 11110.37-110.37 110.5 110.5 0 01-110.37 110.37z" />
+						</svg>
+					</NavigationMenuLink>
+				</Link>
 			</NavigationMenu>
 		</header>
 	);
