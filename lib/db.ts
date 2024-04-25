3c5da39bd50f2989f466ed0d4dc2af61744cb663
LoV432 <monib619@gmail.com>
2024-04-25 20:50:08 +0500

    fix message created_at

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,12 @@
 import pg from 'pg';
 const { Pool } = pg;
 
+pg.types.setTypeParser(1114, function (stringValue) {
+	// TODO: Better way to do this?
+	const date = new Date(stringValue);
+	return new Date(date.getTime() - date.getTimezoneOffset() * 60000);
+});
+
 export const db = new Pool({
 	user: process.env.PG_USER,
 	host: process.env.PG_HOST,
