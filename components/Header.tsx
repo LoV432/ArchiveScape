15778357562738278d5c34968fffad86dd0475f4
LoV432 <monib619@gmail.com>
2024-04-28 03:32:32 +0500

    update mobile navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
 	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<header className="sticky top-0 z-10 mb-2 w-full border-b border-muted bg-background px-2 py-2 sm:px-5">
-			<NavigationMenu className="hidden w-full max-w-[initial] gap-2 sm:flex">
+			<NavigationMenu className="hidden w-full max-w-[initial] gap-4 sm:flex">
 				<Link href="/" legacyBehavior passHref>
 					<NavigationMenuLink>
 						<Image
@@ -41,7 +41,7 @@ export default function Header() {
 					<NavigationMenuItem>
 						<Link href="/" legacyBehavior passHref>
 							<NavigationMenuLink
-								className={`${navigationMenuTriggerStyle()} text-xl font-bold tracking-widest`}
+								className={`mr-5 text-xl font-bold tracking-widest`}
 							>
 								ArchiveScape
 							</NavigationMenuLink>
@@ -80,19 +80,51 @@ export default function Header() {
 					</NavigationMenuLink>
 				</Link>
 			</NavigationMenu>
-			<NavigationMenu className="w-full max-w-full justify-start sm:hidden">
+			<NavigationMenu className="w-full max-w-full justify-start gap-3 py-1 sm:hidden">
+				<Link href="/" legacyBehavior passHref>
+					<NavigationMenuLink>
+						<Image
+							priority
+							className="h-6 w-6"
+							src="/logo.png"
+							alt="logo"
+							width={24}
+							height={24}
+						/>
+					</NavigationMenuLink>
+				</Link>
+				<Link href="/" legacyBehavior passHref>
+					<NavigationMenuLink
+						className={`mr-auto text-center text-xl font-bold tracking-widest`}
+					>
+						ArchiveScape
+					</NavigationMenuLink>
+				</Link>
+				<Link href="/search" legacyBehavior passHref>
+					<NavigationMenuLink
+						className={`mr-2.5 text-center text-xl font-bold`}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="ionicon h-6 w-6 fill-white"
+							viewBox="0 0 512 512"
+						>
+							<path d="M464 428L339.92 303.9a160.48 160.48 0 0030.72-94.58C370.64 120.37 298.27 48 209.32 48S48 120.37 48 209.32s72.37 161.32 161.32 161.32a160.48 160.48 0 0094.58-30.72L428 464zM209.32 319.69a110.38 110.38 0 11110.37-110.37 110.5 110.5 0 01-110.37 110.37z" />
+						</svg>
+					</NavigationMenuLink>
+				</Link>
 				<Sheet open={isOpen} onOpenChange={setIsOpen}>
 					<SheetTrigger>
 						<Image
 							priority
-							className="h-6 w-6"
+							className="h-7 w-7"
 							src="/list-outline.svg"
 							alt="logo"
 							width={24}
 							height={24}
 						/>
 					</SheetTrigger>
-					<SheetContent className="w-fit min-w-[300px]" side="left">
+					<SheetContent className="w-fit min-w-[300px]" side="right">
 						<SheetHeader>
 							<SheetDescription className="pt-2">
 								<NavigationMenuList className="float-left flex-col gap-5 space-x-0">
@@ -141,26 +173,6 @@ export default function Header() {
 						</SheetHeader>
 					</SheetContent>
 				</Sheet>
-				<Link href="/" legacyBehavior passHref>
-					<NavigationMenuLink
-						className={`${navigationMenuTriggerStyle()} mx-auto text-center text-xl font-bold tracking-widest`}
-					>
-						ArchiveScape
-					</NavigationMenuLink>
-				</Link>
-				<Link href="/search" legacyBehavior passHref>
-					<NavigationMenuLink
-						className={`${navigationMenuTriggerStyle()} text-center text-xl font-bold`}
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="ionicon h-6 w-6 fill-white"
-							viewBox="0 0 512 512"
-						>
-							<path d="M464 428L339.92 303.9a160.48 160.48 0 0030.72-94.58C370.64 120.37 298.27 48 209.32 48S48 120.37 48 209.32s72.37 161.32 161.32 161.32a160.48 160.48 0 0094.58-30.72L428 464zM209.32 319.69a110.38 110.38 0 11110.37-110.37 110.5 110.5 0 01-110.37 110.37z" />
-						</svg>
-					</NavigationMenuLink>
-				</Link>
 			</NavigationMenu>
 		</header>
 	);
