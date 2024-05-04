663a31610360a57f4247f85a34bc576da61681d7
LoV432 <monib619@gmail.com>
2024-05-05 02:37:19 +0500

    update loading ui

--- components/LoadingOverlay.tsx
+++ components/LoadingOverlay.tsx
@@ -1,6 +1,6 @@
 export default function LoadingOverlay() {
 	return (
-		<div className="absolute left-0 top-0 z-50 h-full w-full bg-neutral-800 bg-opacity-60">
+		<div className="fixed left-0 top-0 z-50 h-full w-full bg-neutral-800 bg-opacity-60">
 			<div className="grid h-full place-items-center">
 				<div className="lds-ripple">
 					<div></div>
