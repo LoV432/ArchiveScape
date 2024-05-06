42ead7ac85c399a472dc6f4b377822d28d2d54b8
LoV432 <monib619@gmail.com>
2024-05-06 18:21:57 +0500

    fix slow navigation in /stats/links page

--- app/stats/links/page.tsx
+++ app/stats/links/page.tsx
@@ -1,14 +1,6 @@
-import LoadingOverlay from '@/components/LoadingOverlay';
-import { Suspense } from 'react';
 import AllMessagesWithLinks from './page.client';
-import { db } from '@/lib/db';
-import { Metadata } from 'next/types';
+import { getTopDomain } from '@/lib/top-domain';
 export const dynamic = 'force-dynamic';
-
-export const metadata: Metadata = {
-	title: 'Links Sent By Users | ArchiveScape',
-	description: 'An archive of all messages sent on https://www.ventscape.life/'
-};
 
 export default async function Page() {
 	const topDomain = await getTopDomain();
@@ -17,40 +9,10 @@ export default async function Page() {
 			<h1 className="text-center text-2xl text-rose-700 sm:text-5xl">
 				Most Sent Domain:
 			</h1>
-			<Suspense fallback={<LoadingOverlay />}>
-				<p className="pb-3 text-center text-2xl text-rose-700 sm:text-5xl">
-					{topDomain.domain}
-				</p>
-			</Suspense>
+			<p className="pb-3 text-center text-2xl text-rose-700 sm:text-5xl">
+				{topDomain.domain}
+			</p>
 			<AllMessagesWithLinks />
 		</main>
 	);
-}
-
-async function getTopDomain() {
-	const messagesWithLinks = (
-		await db.query(`SELECT message_text FROM messages WHERE message_text ~ 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+';
-    `)
-	).rows as {
-		message_text: string;
-	}[];
-
-	const links = messagesWithLinks
-		.map((row) =>
-			row.message_text.match(
-				'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-			)
-		)
-		.flat()
-		.sort();
-	let rankedLinksObject: Record<string, number> = {};
-	links.forEach((link) => {
-		if (!link) return;
-		rankedLinksObject[link.split('/')[2]] =
-			(rankedLinksObject[link.split('/')[2]] || 0) + 1;
-	});
-	const rankedLinks = Object.entries(rankedLinksObject).sort(
-		(a, b) => b[1] - a[1]
-	);
-	return { domain: rankedLinks[0][0], count: rankedLinks[0][1] };
 }
---
+++ lib/top-domain.tsx
@@ -0,0 +1,43 @@
+import { db } from './db';
+
+let lastUpdated = 0;
+let topDomainCache: { domain: string; count: number };
+export async function getTopDomain() {
+	const now = Date.now();
+	if (now - lastUpdated < 1000 * 60 * 5 && topDomainCache.count > 0) {
+		return {
+			domain: topDomainCache.domain,
+			count: topDomainCache.count
+		};
+	}
+	lastUpdated = now;
+	const messagesWithLinks = (
+		await db.query(`SELECT message_text FROM messages WHERE message_text ~ 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+';
+    `)
+	).rows as {
+		message_text: string;
+	}[];
+
+	const links = messagesWithLinks
+		.map((row) =>
+			row.message_text.match(
+				'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+			)
+		)
+		.flat()
+		.sort();
+	let rankedLinksObject: Record<string, number> = {};
+	links.forEach((link) => {
+		if (!link) return;
+		rankedLinksObject[link.split('/')[2]] =
+			(rankedLinksObject[link.split('/')[2]] || 0) + 1;
+	});
+	const rankedLinks = Object.entries(rankedLinksObject).sort(
+		(a, b) => b[1] - a[1]
+	);
+	topDomainCache = {
+		domain: rankedLinks[0][0],
+		count: rankedLinks[0][1]
+	}
+	return { domain: rankedLinks[0][0], count: rankedLinks[0][1] };
+}
