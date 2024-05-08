06a69d90662a473f777943663e415b3ed1fc17af
LoV432 <monib619@gmail.com>
2024-05-08 17:41:15 +0500

    ssr homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-import { useQuery } from '@tanstack/react-query';
 import {
 	Table,
 	TableBody,
@@ -11,64 +9,51 @@ import {
 } from '@/components/ui/table';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import { db } from '@/lib/db';
 
-export default function Home() {
-	const query = useQuery({
-		queryKey: ['top-users'],
-		queryFn: async () => {
-			const res = await fetch('/api/top-users');
-			if (!res.ok) {
-				throw new Error('Error');
-			}
-			return (await res.json()) as {
-				topUsers: {
-					user_name: string;
-					user_id: number;
-					message_count: number;
-				}[];
-			};
-		}
-	});
+export default async function Home() {
+	const data = (
+		await db.query(`SELECT u.user_name, m.user_id, COUNT(*) AS message_count
+	FROM messages m
+	JOIN users u ON m.user_id = u.id
+	GROUP BY u.user_name, m.user_id
+	ORDER BY message_count DESC
+	LIMIT 10`)
+	).rows as { user_name: string; user_id: number; message_count: number }[];
 
 	return (
 		<main className="grid">
 			<h1 className="place-self-center py-5 text-4xl font-bold tracking-widest sm:text-6xl">
 				TOP 10 USERS
 			</h1>
-			{query.isLoading && <div className="place-self-center">Loading...</div>}
-			{query.isError && <p>Error</p>}
-			{query.isSuccess && (
-				<>
-					<Table className="mx-auto max-w-3xl">
-						<TableCaption hidden>Top Users</TableCaption>
-						<TableHeader>
-							<TableRow>
-								<TableHead>User</TableHead>
-								<TableHead className="text-right">Message Count</TableHead>
-							</TableRow>
-						</TableHeader>
-						<TableBody>
-							{query.data.topUsers.map((user, index) => (
-								<TableRow className="relative" key={user.user_id}>
-									<TableCell
-										className={`block max-w-[calc(100vw/1.5)] overflow-hidden text-ellipsis font-medium ${colorGradientByIndex(index)}`}
-									>
-										<Link
-											href={`/users/${user.user_id}/messages`}
-											className="text-base before:absolute before:left-0 before:top-0 before:h-full before:w-full sm:text-lg"
-										>
-											{user.user_name}
-										</Link>
-									</TableCell>
-									<TableCell className="text-right text-base font-medium sm:text-lg">
-										{user.message_count}
-									</TableCell>
-								</TableRow>
-							))}
-						</TableBody>
-					</Table>
-				</>
-			)}
+			<Table className="mx-auto max-w-3xl">
+				<TableCaption hidden>Top Users</TableCaption>
+				<TableHeader>
+					<TableRow>
+						<TableHead>User</TableHead>
+						<TableHead className="text-right">Message Count</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{data.map((user, index) => (
+						<TableRow className="relative" key={user.user_id}>
+							<TableCell
+								className={`block max-w-[calc(100vw/1.5)] overflow-hidden text-ellipsis font-medium ${colorGradientByIndex(index)}`}
+							>
+								<Link
+									href={`/users/${user.user_id}/messages`}
+									className="text-base before:absolute before:left-0 before:top-0 before:h-full before:w-full sm:text-lg"
+								>
+									{user.user_name}
+								</Link>
+							</TableCell>
+							<TableCell className="text-right text-base font-medium sm:text-lg">
+								{user.message_count}
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
 			<Link className="my-3 justify-self-center" href="/users">
 				<Button tabIndex={-1} variant="outline">
 					Show All
