065229da86ee365ae3ecfb15aceb20d83b93651c
LoV432 <monib619@gmail.com>
2024-05-10 22:43:22 +0500

    remove date/time fix
    not required after moving from timestamp to timestampz on db

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,6 @@
 import pg from 'pg';
 const { Pool } = pg;
 
-pg.types.setTypeParser(1114, function (stringValue) {
-	// TODO: This seems like a better way to do this, but is it?
-	// https://stackoverflow.com/questions/20712291/use-node-postgres-to-get-postgres-timestamp-without-timezone-in-utc
-	return new Date(`${stringValue} +0000`);
-});
-
 export const db = new Pool({
 	user: process.env.PG_USER,
 	host: process.env.PG_HOST,
