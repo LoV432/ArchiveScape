6562ab6b133d4c3c73aca5e94f815c9745d04518
LoV432 <monib619@gmail.com>
2024-05-30 00:28:19 +0500

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,54 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	return [
+		{
+			url: `https://${process.env.HOST_NAME}/`,
+			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 1
+		},
+		{
+			url: `https://${process.env.HOST_NAME}/users`,
+			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 0.5
+		},
+		{
+			url: `https://${process.env.HOST_NAME}/all-messages`,
+			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 0.5
+		},
+		{
+			url: `https://${process.env.HOST_NAME}/replay`,
+			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 0.5
+		},
+		{
+			url: `https://${process.env.HOST_NAME}/stats/words-cloud`,
+			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 0.5
+		},
+		{
+			url: `https://${process.env.HOST_NAME}/stats/links`,
+			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 0.5
+		},
+		{
+			url: `https://${process.env.HOST_NAME}/stats/emojis`,
+			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 0.5
+		},
+		{
+			url: `https://${process.env.HOST_NAME}/stats/rickroll`,
+			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 0.5
+		}
+	];
+}
