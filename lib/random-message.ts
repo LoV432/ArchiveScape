1975604471e3031d9d1dc66a9ab2f3bfe8b72d71
LoV432 <monib619@gmail.com>
2024-07-04 23:00:24 +0500

    update random message query

--- lib/random-message.ts
+++ lib/random-message.ts
@@ -6,16 +6,26 @@ export type RandomMessage = {
 };
 
 export async function getRandomMessage() {
+	const totalUsers = (
+		await db.query(`SELECT id as count FROM users order by id desc limit 1`)
+	).rows[0].count;
+	const randomOffset = Math.floor(Math.random() * (totalUsers - 1000));
+	// We are calculating offset like this to avoid using random() sort in DB
+	// This drops total time from 350ms~ to 50ms~
 	const user = (
-		await db.query(`SELECT m.user_id
-        FROM messages m
-        JOIN users u ON m.user_id = u.id
-        GROUP BY m.user_id
-        HAVING COUNT(*) < 5 ORDER BY RANDOM() LIMIT 1`)
+		await db.query(
+			`SELECT m.user_id
+			FROM messages m
+			JOIN users u ON m.user_id = u.id
+			WHERE m.user_id > $1
+			GROUP BY m.user_id
+			HAVING COUNT(*) < 5 limit 1`,
+			[randomOffset]
+		)
 	).rows[0];
 	const message = (
 		await db.query(
-			`SELECT message_text, colors.color_name FROM messages LEFT JOIN colors ON messages.color_id = colors.id WHERE user_id = $1`,
+			`SELECT message_text, colors.color_name, user_id FROM messages LEFT JOIN colors ON messages.color_id = colors.id WHERE user_id = $1`,
 			[user.user_id]
 		)
 	).rows as RandomMessage[];
