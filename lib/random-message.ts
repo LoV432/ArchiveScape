f1836d8477d6325cfa18c4f465a74d13637360ad
LoV432 <monib619@gmail.com>
2024-07-13 00:13:21 +0500

    better? quality random messages

--- lib/random-message.ts
+++ lib/random-message.ts
@@ -12,25 +12,36 @@ export async function getRandomMessage() {
 	const randomOffset = Math.floor(Math.random() * (totalUsers - 1000));
 	// We are calculating offset like this to avoid using random() sort in DB
 	// This drops total time from 350ms~ to 50ms~
-	const user = (
+	const users = (
 		await db.query(
 			`SELECT m.user_id
 			FROM messages m
 			JOIN users u ON m.user_id = u.id
 			WHERE m.user_id > $1
 			GROUP BY m.user_id
-			HAVING COUNT(*) < 5 limit 1`,
+			HAVING COUNT(*) < 3 limit 10`,
 			[randomOffset]
 		)
-	).rows[0];
-	const message = (
+	).rows;
+	const messages = (
 		await db.query(
-			`SELECT message_text, colors.color_name, user_id FROM messages LEFT JOIN colors ON messages.color_id = colors.id WHERE user_id = $1`,
-			[user.user_id]
+			`SELECT message_text, colors.color_name, user_id FROM messages LEFT JOIN colors ON messages.color_id = colors.id WHERE user_id = ANY($1::int[])`,
+			[users.map((user) => user.user_id)]
 		)
 	).rows as RandomMessage[];
+	let bestMessage = messages[0];
+	for (const message of messages) {
+		// TODO: Somehow filter out obvious troll messages, maybe using LLM?
+		if (Math.random() < 0.05 && message.message_text.length > 20) {
+			bestMessage = message;
+			break;
+		}
+		if (message.message_text.length > bestMessage.message_text.length) {
+			bestMessage = message;
+		}
+	}
 	return {
-		message_text: message[1]?.message_text || message[0].message_text,
-		color_name: message[1]?.color_name || message[0].color_name
+		message_text: bestMessage.message_text,
+		color_name: bestMessage.color_name
 	};
 }
