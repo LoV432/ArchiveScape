0daaa52314fa3ab4f22ff71b576ff1ddfd70d5d2
LoV432 <monib619@gmail.com>
2024-08-30 23:55:06 +0500

    fix images in prod

--- next.config.prod.mjs
+++ next.config.prod.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'standalone' };
+const nextConfig = {
+	output: 'standalone',
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'api.dicebear.com'
+			}
+		]
+	}
+};
 
 export default nextConfig;
