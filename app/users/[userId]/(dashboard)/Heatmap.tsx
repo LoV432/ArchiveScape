6a8f428c4e1d4020748bb5f4fa5c387eccd71725
LoV432 <monib619@gmail.com>
2024-09-06 23:38:52 +0500

    fix heatmap loading stub

--- app/users/[userId]/(dashboard)/Heatmap.tsx
+++ app/users/[userId]/(dashboard)/Heatmap.tsx
@@ -103,19 +103,21 @@ export default function Heatmap({
 
 export function HeatMapLoading() {
 	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			strokeWidth="2"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-			className="animate-spin"
-		>
-			<path d="M21 12a9 9 0 1 1-6.219-8.56" />
-		</svg>
+		<div className="mx-auto grid h-[152px] max-w-3xl place-items-center overflow-scroll rounded-sm border p-4 shadow-md">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				className="animate-spin"
+			>
+				<path d="M21 12a9 9 0 1 1-6.219-8.56" />
+			</svg>
+		</div>
 	);
 }
