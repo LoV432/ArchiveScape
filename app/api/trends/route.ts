971c41b588b40a3a603c03a2ce3d257d93404e48
LoV432 <monib619@gmail.com>
2024-09-15 17:46:00 +0500

    fix trends api

--- app/api/trends/route.ts
+++ app/api/trends/route.ts
@@ -1,5 +1,6 @@
 import { getTrendsData } from '@/app/stats/trends/trends-data';
 import { NextRequest, NextResponse } from 'next/server';
+export const dynamic = 'force-dynamic';
 
 export async function GET(request: NextRequest) {
 	try {
