f16c476604b5ba7c6fbbe23b257c017af6f0c370
LoV432 <monib619@gmail.com>
2024-09-16 18:00:33 +0500

    trends: add meta tags

--- app/stats/trends/page.tsx
+++ app/stats/trends/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next/types';
 import { TrendsChart } from './TrendsChart';
+
+export const metadata: Metadata = {
+	title: 'Word Trends | ArchiveScape',
+	description: 'An archive of all messages sent on https://www.ventscape.life/'
+};
 
 export default function Page() {
 	return <TrendsChart />;
