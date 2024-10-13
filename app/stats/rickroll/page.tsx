edc35e4e73a95d2916e5c62f9ec4e9be1ed1b342
LoV432 <monib619@gmail.com>
2024-10-13 17:56:02 +0500

    optimize rickroll query

--- app/stats/rickroll/page.tsx
+++ app/stats/rickroll/page.tsx
@@ -10,26 +10,38 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-	const lastDate = await db.query(
-		`SELECT *, users.user_name, colors.color_name, messages.id FROM messages LEFT JOIN colors ON messages.color_id = colors.id JOIN users ON messages.user_id = users.id  WHERE message_text LIKE '%dQw4w9WgXcQ%' ORDER BY created_at DESC LIMIT 1`
-	);
+	const lastDate = (
+		await db.query(
+			`SELECT messages.id as id, messages.created_at as created_at, users.id as user_id, users.user_name as user_name, colors.color_name as color_name FROM messages
+		 	LEFT JOIN colors ON messages.color_id = colors.id JOIN users ON messages.user_id = users.id 
+		 	WHERE message_text LIKE '%dQw4w9WgXcQ%' 
+			ORDER BY created_at + interval '0 second' DESC 
+			LIMIT 1`
+		)
+	).rows[0] as {
+		id: number;
+		created_at: Date;
+		user_id: number;
+		color_name: string;
+		user_name: string;
+	};
 	return (
 		<div className="mx-5 flex h-full max-w-[100vw] flex-col place-items-center justify-center gap-7 text-center sm:gap-10">
 			<p className="break-words text-4xl font-extrabold sm:text-7xl">
 				Last Rickroll:
 			</p>
 			<div className="break-words text-2xl font-extrabold sm:text-4xl">
-				<ClientPage time={lastDate.rows[0].created_at} />
+				<ClientPage time={lastDate.created_at} />
 			</div>
 			<p className="text-2xl font-extrabold sm:text-4xl">By</p>
 			<Link
-				href={`/users/${lastDate.rows[0].user_id}/messages/${lastDate.rows[0].id}/message-context`}
+				href={`/users/${lastDate.user_id}/messages/${lastDate.id}/message-context`}
 				style={{
-					color: lastDate.rows[0].color_name
+					color: lastDate.color_name
 				}}
 				className="text-xl font-extrabold text-[var(--highlight)] sm:text-4xl"
 			>
-				{lastDate.rows[0].user_name}
+				{lastDate.user_name}
 			</Link>
 			<Link
 				target="_blank"
