5ec555196a07b8641ba01e4504f9bd64772f4ab2
LoV432 <monib619@gmail.com>
2024-12-22 01:11:03 +0500

    dont sort all users page by total messages

--- lib/all-users.tsx
+++ lib/all-users.tsx
@@ -9,17 +9,18 @@ export type User = {
 export async function getAllUsers(page: number) {
 	const itemsPerPage = Number(process.env.ITEMS_PER_PAGE) || 10;
 	const usersWithMessagesCount = await db.query(
+		// TODO: Pagination will be an issue now that we are not sorting by total message count and showing most recent first
+		// We might need to do some DB pinning for this too or we could simply do cursor pagination
 		`SELECT u.user_name, m.message_count, m.user_id as id
 			FROM users u
 			INNER JOIN (
 				SELECT COUNT(*) AS message_count, user_id
 				FROM messages
 				GROUP BY user_id
-				ORDER BY message_count desc
-				OFFSET $1
-				LIMIT $2
 			) m ON u.id = m.user_id
-			ORDER BY m.message_count DESC;`,
+			ORDER BY u.id DESC
+			OFFSET $1
+			LIMIT $2;`,
 		[Number(page) * itemsPerPage - itemsPerPage, itemsPerPage]
 	);
 	return { users: usersWithMessagesCount.rows as User[] };
