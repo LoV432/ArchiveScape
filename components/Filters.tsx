08f0ad67ebc17bd5b1c8617f0a3f6f3589071a40
LoV432 <monib619@gmail.com>
2025-01-20 15:28:07 +0500

    remove next13-progressbar

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -15,8 +15,7 @@ import { useEffect, useState } from 'react';
 import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Button } from '@/components/ui/button';
-import { usePathname, useSearchParams } from 'next/navigation';
-import { useRouter } from 'next13-progressbar';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 import { ListFilter } from 'lucide-react';
 
 export function Filters() {
