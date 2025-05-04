b6c03e757f7684fc2959955cd17215d0e4b10201
LoV432 <monib619@gmail.com>
2025-05-04 23:12:43 +0500

    disallow bingbot on /search using robots.txt
    bing spams random search queries
    
    https://news.ycombinator.com/item?id=43342821
    https://www.webmasterworld.com/msn_microsoft_search/5117662.htm

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,16 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
 	return {
-		rules: {
-			userAgent: '*',
-			allow: '/'
-		},
+		rules: [
+			{
+				userAgent: 'bingbot',
+				disallow: '/search'
+			},
+			{
+				userAgent: '*',
+				allow: '/'
+			}
+		],
 		sitemap: `https://${process.env.HOST_NAME}/sitemap.xml`
 	};
 }
